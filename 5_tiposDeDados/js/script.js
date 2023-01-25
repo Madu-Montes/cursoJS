2be500a65b46c86b439e6780531ed1c8ff222023
Madu-Montes <monteseduarda56@gmail.com>
2023-01-25 16:35:57 -0300

    aula 6

---
+++ 5_tiposDeDados/js/script.js
@@ -0,0 +1,13 @@
+var numero = 5;
+
+console.log(numero);
+console.log(typeof numero);
+
+var float = 5.32;
+console.log(float);
+console.log(typeof float);
+
+var textWithNumber = '523'; //como esta entre aspas ser torna texto
+console.log(textWithNumber);
+console.log(typeof textWithNumber); 
+
