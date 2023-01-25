c8a95c6661bbde214e84b9d910a65d26db90d23e
Madu-Montes <monteseduarda56@gmail.com>
2023-01-25 16:48:17 -0300

    aula 6

---
+++ 6_string/js/script.js
@@ -0,0 +1,15 @@
+var nome ='Madu';
+var sobrenome ="Montes";
+var frase = "é Front-end"
+
+//concatenação
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log("Brasil");
+console.log(typeof "Brasil");
+console.log(nomeCompleto);
+console.log(nome + " " +  frase);
